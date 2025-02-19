f0303f89a1a18489ab755a3b44cb39212c710738
lucas.dalan <lucasdalan09@gmail.com>
2025-02-19 09:59:56 -0300

    fix: fixed imports paths

--- src/rooming-list-booking/rooming-list-booking.module.ts
+++ src/rooming-list-booking/rooming-list-booking.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { RoomingListBookingService } from "./rooming-list-booking.service";
 import { RoomingListBookingController } from "./rooming-list-booking.controller";
-import { DatabaseModule } from "src/database/database.module";
+import { DatabaseModule } from "../database/database.module";
 
 @Module({
   imports: [DatabaseModule],
--- src/rooming-lists/rooming-lists.module.ts
+++ src/rooming-lists/rooming-lists.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { RoomingListsService } from "./rooming-lists.service";
 import { RoomingListsController } from "./rooming-lists.controller";
-import { DatabaseModule } from "src/database/database.module";
+import { DatabaseModule } from "../database/database.module";
 
 @Module({
   imports: [DatabaseModule],
